9cfc1dc9214b8ea4ec306e055e3ca58852af626a
Cory <coryrosser@gmail.com>
2020-09-11 17:59:02 -0400

    contact file made

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+import {Row, Col, Image} from 'react-bootstrap'
+const Styles = styled.div``
+
+const Contact = (props) => {
+  return (
+    <Styles>
+      Contact Here
+    </Styles>
+  )
+}
+export default Contact
